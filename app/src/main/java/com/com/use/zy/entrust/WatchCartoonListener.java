bb6f2385b178c700615d9c3e02e6b1722a35a778
jfzouzou <jfzouzou@163.com>
2016-07-18 09:05:08 +0800

    Initial commit

---
+++ app/src/main/java/com/com/use/zy/entrust/WatchCartoonListener.java
@@ -0,0 +1,14 @@
+package com.com.use.zy.entrust;
+
+import java.util.Date;
+
+public class WatchCartoonListener {   
+    public WatchCartoonListener(){   
+        System.out.println(this.getClass().getSimpleName()
+        		+"  我正在看漫画，开始时间："+new Date());   
+    }   
+    public void stopPlayingGame(Date date){   
+        System.out.println(this.getClass().getSimpleName()
+        		+"    老师来了，不要看漫画了，结束时间："+date);   
+    }   
+}  
---
+++ app/src/main/java/com/use/zy/observer/Observer.java
@@ -0,0 +1,18 @@
+package com.use.zy.observer;
+
+/**
+ * 观察者接口
+ * @author Administrator
+ *
+ */
+public interface Observer {
+	
+	/**
+	 * 
+	 * @param subject 被观察者
+	 * @param data    被观察者传递给观察者的 数据
+	 */
+	void update(Subject subject, Object data);
+	
+
+}
